65e666f9570e2e846b5b4c91a47a2e9b192a7505
Pooja Hooda <91055527+poojahooda22@users.noreply.github.com>
2024-03-26 20:50:17 +0530

    updated heading

--- src/components/LandingPage.tsx
+++ src/components/LandingPage.tsx
@@ -2,8 +2,19 @@
 
 function LandingPage() {
   return (
-    <div className="w-full h-screen">
-      
+    <div className="w-full h-screen pt-1">
+      <div className="textstructure mt-40 px-20">
+        {["We Create", "Eye-Opening", "Presentations"].map((text, index) => {
+            return (
+                <div className="masker ">
+            <h1 className="uppercase text-9xl tracking-tighter leading-none font-['Founders_Grotesk'] font-semibold">
+                We Create
+            </h1>
+        </div>
+            )
+        })}
+        
+      </div>
     </div>
   )
 }
