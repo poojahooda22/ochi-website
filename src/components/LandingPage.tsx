ed3126fbafcbaab3c9f9e5b28b83969222c7539a
Pooja Hooda <91055527+poojahooda22@users.noreply.github.com>
2024-03-26 22:55:39 +0530

    updated page

--- src/components/LandingPage.tsx
+++ src/components/LandingPage.tsx
@@ -5,11 +5,11 @@ import styles from "./landing.module.scss";
 function LandingPage() {
   return (
     <div className="w-full h-screen pt-1">
-      <div className="textstructure mt-52 px-8">
+      <div className="textstructure mt-40 px-8">
         {["We Create", "Eye-Opening", "Presentations"].map((text, index) => {
             return (
                 <div key={index} className="masker ">
-                    <h1 className={`uppercase text-[6vw] tracking-tighter leading-[5.5vw] font-semibold`}>
+                    <h1 className={`flex items-center h-full uppercase text-[6vw] tracking-tighter leading-[5.5vw] font-bold`}>
                         {text}
                         {/* {index === 1 && <div> <img src={image } /></div>} */}
                     </h1>
@@ -18,7 +18,7 @@ function LandingPage() {
         })}  
       </div>
 
-      <div className="border-t-[1px]  border-zinc-400 mt-32 flex justify-between items-center py-4 px-8 ">
+      <div className="border-t-[1px]  border-zinc-400 mt-40 flex justify-between items-center py-4 px-8 ">
         {["For public and private companies", "From the first pitch to IPO"].map((item, index) => {
             return (
                 <h4 key={index} className="text-gray-900 text-sm font-normal tracking-tight leading-none" >
