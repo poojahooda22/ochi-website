4eac4d95f9d8cdadd6067993f50a3e1938ca5d41
Pooja Hooda <91055527+poojahooda22@users.noreply.github.com>
2024-03-26 22:43:18 +0530

    updated page

--- src/components/LandingPage.tsx
+++ src/components/LandingPage.tsx
@@ -3,7 +3,7 @@
 function LandingPage() {
   return (
     <div className="w-full h-screen pt-1">
-      <div className="textstructure mt-52 px-16">
+      <div className="textstructure mt-52 px-8">
         {["We Create", "Eye-Opening", "Presentations"].map((text, index) => {
             return (
                 <div key={index} className="masker ">
@@ -16,16 +16,19 @@ function LandingPage() {
         })}  
       </div>
 
-      <div className="border-t-[1px] border-zinc-400 mt-32 flex justify-between items-center ">
+      <div className="border-t-[1px]  border-zinc-400 mt-32 flex justify-between items-center py-4 px-8 ">
         {["For public and private companies", "From the first pitch to IPO"].map((item, index) => {
             return (
-                <h4 key={index} className="text-md font-light tracking-tight leading-none" >
+                <h4 key={index} className="text-gray-900 text-sm font-normal tracking-tight leading-none" >
                     {item}
                 </h4>
             )
         })}
         <div>
-
+            <h4 className="relative text-gray-900 text-sm  px-[18px] 
+                py-[6px] border-[1px] border-[#333] rounded-[50px] overflow-hidden">
+                <a>Start project</a>
+            </h4>
         </div>
       </div>
     </div>
