f2731740b4ccd747aa195ccfc1e78597b8ad492a
Pooja Hooda <91055527+poojahooda22@users.noreply.github.com>
2024-03-27 11:35:24 +0530

    created about

---
+++ src/components/About.tsx
@@ -0,0 +1,15 @@
+
+
+function About() {
+  return (
+    <div className="w-full py-20 px-8 bg-[#CDEA68] rounded-tl-3xl rounded-tr-3xl">
+      <h1 className="text-[4vw] leading-[4.5vw] tracking-tight">
+      Ochi is a strategic partner for fast-grow­ing tech 
+      businesses that need to raise funds, sell prod­ucts, ex­plain com­plex ideas,
+       and hire great peo­ple.
+      </h1>
+    </div>
+  )
+}
+
+export default About
