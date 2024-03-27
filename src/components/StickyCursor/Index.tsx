47fa59bf1a2d78882da37e0405dbdcea8e2caa4e
Pooja Hooda <91055527+poojahooda22@users.noreply.github.com>
2024-03-27 19:47:52 +0530

    updated index

--- src/components/StickyCursor/Index.tsx
+++ src/components/StickyCursor/Index.tsx
@@ -62,7 +62,7 @@ export default function Index({stickyElement}: any) {
   }
   const manageMouseLeave = e => {
     setIsHovered(false);
-    animate(cursorRef.current, { scaleX: 1, scaleY: 1 }, {duration: 0.1}, {type: "spring"})
+    animate(cursorRef.current, { scaleX: 1, scaleY: 1 }, {duration: 0.3}, {type: "spring"})
   }
 
   useEffect( () => {
