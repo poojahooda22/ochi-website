d964bc11bbb1eb4b40e1353112381942f1b014a8
Pooja Hooda <91055527+poojahooda22@users.noreply.github.com>
2024-03-27 21:50:29 +0530

    updated feature

--- src/components/Featured.tsx
+++ src/components/Featured.tsx
@@ -4,11 +4,7 @@ function Featured() {
   return (
     <div className='w-full py-20'>
       <div className='w-full px-8 border-b-[1px] border-zinc pb-10'>
-        <h1 className='text-4xl font-medium tracking-tight mb-8'>Featured Projects</h1>
-        <div className='cards w-full flex gap-10'>
-            <div className='card w-1/2 h-[60vh] bg-red-500'></div>
-            <div className='card w-1/2 h-[60vh] bg-red-500'></div>
-        </div>
+        <h1 className='text-4xl font-medium tracking-tight mb-8'>Featured Projects</h1>    
       </div>
     </div>
   )
