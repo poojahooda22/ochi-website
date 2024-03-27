f8c68df111e5ea35ff7674f28ca612d4a803c866
Pooja Hooda <91055527+poojahooda22@users.noreply.github.com>
2024-03-27 10:25:39 +0530

    created marqueue

---
+++ src/components/Marqueue.tsx
@@ -0,0 +1,16 @@
+
+
+function Marqueue() {
+  return (
+    <div className='w-full py-20 rounded-tl-3xl rounded-tr-3xl bg-[#004D43]'>
+      <div className="text border-t-2 border-b-2 border-zinc-300 flex gap-10">
+
+        <div className="uppercase font-bold text-[14vw] text-white">
+          We are ochi
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Marqueue
