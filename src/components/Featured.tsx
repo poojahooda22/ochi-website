1506cb7c2bb6b22e2216fab344510c5e91439ab2
Pooja Hooda <91055527+poojahooda22@users.noreply.github.com>
2024-03-27 21:41:06 +0530

    updated featured

--- src/components/Featured.tsx
+++ src/components/Featured.tsx
@@ -1,14 +1,13 @@
+import React from 'react'
 
- 
- function Featured() {
-   return (
-     <div className="w-full py-10">
-       <div className="w-full px-8 border-b-[1px]">
-        <h1 className="text-6xl">Featured projects</h1>
-       </div>
-     </div>
-   )
- }
- 
- export default Featured
- 
+function Featured() {
+  return (
+    <div className='w-full py-20'>
+      <div className='w-full px-8 border-b-[1px] border-zinc pb-10'>
+        <h1 className='text-4xl font-bold'>Featured Projects</h1>
+      </div>
+    </div>
+  )
+}
+
+export default Featured
