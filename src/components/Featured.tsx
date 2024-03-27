9aac09ce89a43bb10cf127857a0517df54880d95
Pooja Hooda <91055527+poojahooda22@users.noreply.github.com>
2024-03-27 21:55:26 +0530

    added

--- src/components/Featured.tsx
+++ src/components/Featured.tsx
@@ -7,10 +7,10 @@ function Featured() {
             <h1 className='text-4xl font-medium tracking-tight '>Featured Projects</h1>    
         </div>
         <div className='px-8'>
-        <div className='cards w-full flex gap-10 '>
-            <div className='card w-1/2 h-[60vh] bg-red-500'></div>
-            <div className='card w-1/2 h-[60vh] bg-red-500'></div>
-        </div>
+            <div className='cards w-full flex gap-10 '>
+                <div className='card w-1/2 h-[60vh] bg-red-500'></div>
+                <div className='card w-1/2 h-[60vh] bg-red-500'></div>
+            </div>
 
         </div>
     </div>
