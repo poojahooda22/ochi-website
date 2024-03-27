bf35df9571faae252821c5c25e9a72bbde39a6b5
Pooja Hooda <91055527+poojahooda22@users.noreply.github.com>
2024-03-27 21:54:45 +0530

    updated feature

--- src/components/Featured.tsx
+++ src/components/Featured.tsx
@@ -3,9 +3,10 @@ import React from 'react'
 function Featured() {
   return (
     <div className='w-full py-20'>
-      <div className='w-full px-8 border-b-[1px] border-zinc pb-10'>
-        <h1 className='text-4xl font-medium tracking-tight mb-8'>Featured Projects</h1>    
-      </div>
+        <div className='w-full px-8 border-b-[1px] border-zinc pb-10 mb-10'>
+            <h1 className='text-4xl font-medium tracking-tight '>Featured Projects</h1>    
+        </div>
+        
     </div>
   )
 }
