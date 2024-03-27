da770e74cc21a631037fb2bf59a9af7ec9785634
Pooja Hooda <91055527+poojahooda22@users.noreply.github.com>
2024-03-27 16:46:59 +0530

    updated eyes

--- src/components/Eyes.tsx
+++ src/components/Eyes.tsx
@@ -1,9 +1,11 @@
+import eyesImg from '../assets/images/eyesImg.jpg'
+
 
 function Eyes() {
   return (
     <div className="eyes w-full h-screen overflow-hidden">
-      <div>
-
+      <div className="w-full h-full bg-cover bg-center bg-[url('https://ochi.design/wp-content/uploads/2022/05/Top-Viewbbcbv-1-scaled.jpg')] ">
+            
       </div>
     </div>
   )
