4cc555b178687ab41fce340649120fdbafd77bdf
Pooja Hooda <91055527+poojahooda22@users.noreply.github.com>
2024-03-27 19:11:48 +0530

    updated

--- src/components/Eyes.tsx
+++ src/components/Eyes.tsx
@@ -1,7 +1,22 @@
 
-
+import { useEffect, useState } from "react"
 
 function Eyes() {
+    const [rotate, setRotate] = useState(0);
+
+    useEffect(() => {
+        window.addEventListener("mousemove", (e) => {
+            const mouseX = e.clientX;
+            const mouseY = e.clientY;
+
+            const deltaX = mouseX - window.innerWidth/2;
+            const deltaY = mouseY - window.innerWidth/2;
+
+            const angle = Math.atan2(deltaX, deltaY) * (180/Math.PI);
+
+            setRotate(angle);
+        })
+    })
   return (
     <div className="eyes w-full h-screen overflow-hidden">
       <div className="relative w-full h-full bg-cover bg-center bg-[url('https://ochi.design/wp-content/uploads/2022/05/Top-Viewbbcbv-1-scaled.jpg')] ">
@@ -9,7 +24,7 @@ function Eyes() {
                  {/* This is the first eye */}
                 <div className="flex items-center justify-center w-[15vw] h-[15vw] rounded-full bg-zinc-100">
                     <div className="relative bg-zinc-900 w-[10vw] h-[10vw]  rounded-full">
-                        <div className="absolute top-1/2 left-1/2 -translate-x-[50%] -translate-y-[50%] line w-full h-10">
+                        <div style={{transform: `translate(-50%, -50%) rotate(${rotate}deg)`}} className="line absolute top-1/2 left-1/2 -translate-x-[50%] -translate-y-[50%] line w-full h-10">
                             <div className=" bg-white w-6 h-6 rounded-full"></div>
                         </div>    
                     </div>
@@ -17,7 +32,7 @@ function Eyes() {
                  {/* This is the second eye */}
                 <div className="flex items-center justify-center w-[15vw] h-[15vw] rounded-full bg-zinc-100">
                 <div className="relative bg-zinc-900 w-[10vw] h-[10vw]  rounded-full">
-                        <div className="absolute top-1/2 left-1/2 -translate-x-[50%] -translate-y-[50%] line w-full h-10">
+                        <div className="line absolute top-1/2 left-1/2 -translate-x-[50%] -translate-y-[50%] line w-full h-10">
                             <div className=" bg-white w-6 h-6 rounded-full"></div>
                         </div>    
                     </div>
