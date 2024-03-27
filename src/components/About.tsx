30620cec4a7ae63780cd8f51701282d6d2e93288
Pooja Hooda <91055527+poojahooda22@users.noreply.github.com>
2024-03-27 12:35:41 +0530

    updated about

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -3,25 +3,25 @@ import image from '../assets/images/about.jpg'
 function About() {
   return (
     <div className="w-full py-20 px-8 bg-[#CDEA68] rounded-tl-3xl rounded-tr-3xl">
-      <h1 className="text-[4vw] leading-[4.5vw] tracking-tight">
-      Ochi is a strategic partner for fast-grow­ing tech 
-      businesses that need to raise funds, sell prod­ucts, ex­plain com­plex ideas,
-       and hire great peo­ple.
-      </h1>
+        <h1 className="text-[4vw] leading-[4.5vw] tracking-tight">
+        Ochi is a strategic partner for fast-grow­ing tech 
+        businesses that need to raise funds, sell prod­ucts, ex­plain com­plex ideas,
+        and hire great peo­ple.
+        </h1>
 
-      <div className="w-full border-t-[1px] mt-24 border-gray-600">
-        <div>
-            <h2>Our approach</h2>
+        <div className="w-full border-t-[1px] mt-24 border-gray-600 flex justify-between">
             <div>
-                <h4 className="">
-                    <a className="">Read More</a>
-                </h4>
+                <h2>Our approach</h2>
+                <div>
+                    <h4 className="">
+                        <a className="">Read More</a>
+                    </h4>
+                </div>
+            </div>
+            <div>
+                <img src={image} />
             </div>
         </div>
-        <div>
-            <img src={image} />
-        </div>
-      </div>
     </div>
   )
 }
