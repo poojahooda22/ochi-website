99ab111d0e5ab848805cca465cbf9ceb4b7853e1
Pooja Hooda <91055527+poojahooda22@users.noreply.github.com>
2024-03-27 17:05:20 +0530

    updated zinc

--- src/components/Eyes.tsx
+++ src/components/Eyes.tsx
@@ -5,13 +5,13 @@ function Eyes() {
   return (
     <div className="eyes w-full h-screen overflow-hidden">
       <div className="relative w-full h-full bg-cover bg-center bg-[url('https://ochi.design/wp-content/uploads/2022/05/Top-Viewbbcbv-1-scaled.jpg')] ">
-            <div className="absolute flex gap-10 items-center justify-center top-1/2 left-1/2 -translate-x-[50%] -translate-y-[50%] w-1/2  ">
+            <div className="absolute flex gap-10 items-center justify-center top-1/2 left-1/2 -translate-x-[50%] -translate-y-[50%]   ">
                  {/* This is the first eye */}
-                <div className="relative w-[15vw] h-[15vw] rounded-full bg-white">
+                <div className="relative w-[15vw] h-[15vw] rounded-full bg-zinc-100">
                     <div className="absolute bg-black w-[10vw] h-[10vw] top-1/2 left-1/2 -translate-x-[50%] -translate-y-[50%] rounded-full"></div>
                 </div>
                  {/* This is the second eye */}
-                <div className="relative w-[15vw] h-[15vw] rounded-full bg-white">
+                <div className="relative w-[15vw] h-[15vw] rounded-full bg-zinc-100">
                     <div className="absolute bg-black w-[10vw] h-[10vw] top-1/2 left-1/2 -translate-x-[50%] -translate-y-[50%] rounded-full"></div>
                 </div>
             </div>
