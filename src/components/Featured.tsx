12d4cd866e5ecafb63b1970444e9f4e3d74c1e7b
Pooja Hooda <91055527+poojahooda22@users.noreply.github.com>
2024-03-27 20:01:11 +0530

    updated featured

---
+++ src/components/Featured.tsx
@@ -0,0 +1,14 @@
+
+ 
+ function Featured() {
+   return (
+     <div className="w-full py-10">
+       <div className="w-full px-8 border-b-[1px]">
+        <h1 className="text-6xl">Featured projects</h1>
+       </div>
+     </div>
+   )
+ }
+ 
+ export default Featured
+ 
