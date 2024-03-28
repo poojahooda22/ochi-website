b5aa8395b4c895280fdde17d2386d7d1f73a12c3
Pooja Hooda <91055527+poojahooda22@users.noreply.github.com>
2024-03-28 20:41:13 +0530

    updated motion

--- src/components/Featured.tsx
+++ src/components/Featured.tsx
@@ -23,12 +23,14 @@ function Featured() {
             <h1 className='text-4xl font-medium tracking-tight '>Featured Projects</h1>    
         </div>
         <div className='px-8'>
-            <div className='cards w-full flex gap-6 '>
+            <div className='cards w-full flex gap-4 '>
                 <motion.div 
+                    whileHover={{ scale: .9 }}
+                    whileTap={{ scale: 1 }}
                     onHoverStart={() => handleHover(0)} 
                     onHoverEnd={() => handleHoverEnd(0)}
                     className='cardcontainer relative w-1/2 h-[75vh]'>                    
-                    <h1 className='absolute flex right-0 translate-x-1/2 overflow-hidden -translate-y-1/2 text-[#CDEA68] top-1/2 text-[5vw] font-semibold font-[Roboto] tracking-tight leading-none z-[9]'>
+                    <h1 className='absolute flex right-0 translate-x-1/2 overflow-hidden -translate-y-1/2 text-[#CDEA68] top-1/2 text-[5vw] font-semibold font-[Roboto] tracking-wide leading-none z-[9]'>
                         {"FYDE".split('').map((item, index) => 
                             (<motion.span 
                                 initial={{y: "100%"}} 
@@ -45,11 +47,20 @@ function Featured() {
                     </div>
                 </motion.div>
                 <motion.div 
+                    onHoverStart={() => handleHover(1)} 
+                    onHoverEnd={() => handleHoverEnd(1)}
                     className='cardcontainer relative w-1/2 h-[75vh]'>
                     <div className='card rounded-lg w-full h-full overflow-hidden'> 
-                        <h1 className='absolute flex right-full translate-x-1/2 -translate-y-1/2 text-[#CDEA68] top-1/2 text-[5vw] font-semibold font-[Roboto] tracking-tight leading-none z-[9]'>
+                        <h1 className='absolute flex right-full translate-x-1/2 overflow-hidden -translate-y-1/2 text-[#CDEA68] top-1/2 text-[5vw] font-semibold font-[Roboto] tracking-wide leading-none z-[9]'>
                             {"VISE".split('').map((item, index) => 
-                                <span key={index}>{item}</span>
+                                (<motion.span 
+                                    initial={{y: "100%"}} 
+                                    animate={cards[1]}
+                                    transition={{ease: [0.22, 1, 0.36, 1], delay: index * .05}}
+                                    className='inline-block'
+                                >
+                                    {item}
+                                </motion.span>)
                             )}
                         </h1>   
                         <img className='w-full h-full bg-cover' src="https://ochi.design/wp-content/uploads/2022/09/Vise_front2-1326x1101.jpg" />
