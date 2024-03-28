ce4bc77680cbfc3d0cfb233f49230242f76718f8
Pooja Hooda <91055527+poojahooda22@users.noreply.github.com>
2024-03-28 23:28:00 +0530

    updated about

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -4,7 +4,7 @@ import styles from './about.module.scss';
 
 function About() {
   return (
-    <div data-scroll data-scroll-section data-scroll-speed="-.2"
+    <div data-scroll data-scroll-section data-scroll-speed="-.1"
     className="w-full py-20 px-8 bg-[#CDEA68] rounded-tl-3xl rounded-tr-3xl">
         <h1 className="text-[4vw] leading-[4.5vw] tracking-tight">
         Ochi is a strategic partner for fast-grow­ing tech 
