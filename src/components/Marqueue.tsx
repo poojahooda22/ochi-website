0c22d3f6c632a01e35baf8c5cd5df1190341c277
Pooja Hooda <91055527+poojahooda22@users.noreply.github.com>
2024-03-28 13:16:58 +0530

    updated loop

--- src/components/Marqueue.tsx
+++ src/components/Marqueue.tsx
@@ -3,9 +3,14 @@ import {motion} from 'framer-motion';
 function Marqueue() {
   return (
     <div className='w-full py-20 rounded-tl-3xl rounded-tr-3xl bg-[#004D43]'>
-      <div className="text border-t-2 border-b-2 border-zinc-300 flex gap-4 overflow-hidden whitespace-nowrap">
+      <div className="text border-t-2 border-b-2 border-zinc-300 flex  overflow-hidden whitespace-nowrap">
 
-        <motion.h1  className="uppercase font-bold text-[20vw] leading-none tracking-tighter text-white">
+        <motion.h1 
+          initial={{x: "0"}}
+          animate={{x: "-100%"}}
+          className="uppercase font-bold text-[20vw] leading-none tracking-tighter mr-20
+          text-white "
+        >
           We are ochi
         </motion.h1>
         <motion.h1 className="uppercase font-bold text-[20vw] leading-none text-white">
