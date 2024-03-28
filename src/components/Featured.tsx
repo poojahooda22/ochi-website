11ecd0c82a77940b69e741b568b376f21848757c
Pooja Hooda <91055527+poojahooda22@users.noreply.github.com>
2024-03-28 16:53:18 +0530

    updated feature

--- src/components/Featured.tsx
+++ src/components/Featured.tsx
@@ -12,7 +12,9 @@ function Featured() {
                 <div className='cardcontainer relative w-1/2 h-[75vh]'>                    
                     <h1 className='absolute flex right-0 translate-x-1/2 -translate-y-1/2 text-[#CDEA68] top-1/2 text-[5vw] font-semibold font-[Roboto] tracking-tight leading-none z-[9]'>
                         {"FYDE".split('').map((item, index) => 
-                            <span key={index}>{item}</span>
+                            <span key={index}>
+                                {item}
+                            </span>
                         )}
                     </h1>  
                     <div className='card rounded-lg w-full h-full overflow-hidden'>
