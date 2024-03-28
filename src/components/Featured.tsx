f85cac27a189f3ec48d4c97f45e0b7249e467927
Pooja Hooda <91055527+poojahooda22@users.noreply.github.com>
2024-03-28 16:41:06 +0530

    updated feature

--- src/components/Featured.tsx
+++ src/components/Featured.tsx
@@ -10,7 +10,13 @@ function Featured() {
             <div className='cards w-full flex gap-6 '>
                 
                 <div className='cardcontainer relative w-1/2 h-[75vh]'>
-                    <h1 className='absolute left-full -translate-x-1/2 -translate-y-1/2 text-[#CDEA68] top-1/2 text-[5vw] font-[Roboto] tracking-tight leading-none z-[9]'>FYDE</h1>
+                    <h1 className='absolute left-full -translate-x-1/2 -translate-y-1/2 text-[#CDEA68] top-1/2 text-[5vw] font-[Roboto] tracking-tight leading-none z-[9]'>
+                    <h1 className='absolute right-full translate-x-1/2 -translate-y-1/2 text-[#CDEA68] top-1/2 text-[5vw] font-[Roboto] tracking-tight leading-none z-[9]'>
+                            {"VISE".split('').map((item, index) => 
+                                <span key={index}>{item}</span>
+                            )}
+                        </h1>  
+                    </h1>
                     <div className='card rounded-lg w-full h-full overflow-hidden'>
                         <img className='w-full h-full bg-cover' src="https://ochi.design/wp-content/uploads/2023/10/Fyde_Illustration_Crypto_2-1326x1101.png" />
                     </div>
