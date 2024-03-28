9c657b1f16569fdd9e3f8df5a29bfbe59c098c42
Pooja Hooda <91055527+poojahooda22@users.noreply.github.com>
2024-03-28 13:09:31 +0530

    updated marqueue

--- src/components/Marqueue.tsx
+++ src/components/Marqueue.tsx
@@ -3,17 +3,15 @@ import {motion} from 'framer-motion';
 function Marqueue() {
   return (
     <div className='w-full py-20 rounded-tl-3xl rounded-tr-3xl bg-[#004D43]'>
-      <div className="text border-t-2 border-b-2 border-zinc-300 flex gap-4 overflow-hidden">
+      <div className="text border-t-2 border-b-2 border-zinc-300 flex gap-4 overflow-hidden whitespace-nowrap">
 
-        <motion.h1  className="uppercase font-bold text-[4vw] leading-none text-white">
+        <motion.h1  className="uppercase font-bold text-[20vw] leading-none tracking-tighter text-white">
           We are ochi
         </motion.h1>
-        <motion.h1 className="uppercase font-bold text-[4vw] leading-none text-white">
+        <motion.h1 className="uppercase font-bold text-[20vw] leading-none text-white">
           We are ochi
         </motion.h1>
-        <motion.h1  className="uppercase font-bold text-[4vw] leading-none text-white">
-          We are ochi
-        </motion.h1>
+        
       </div>
     </div>
   )
