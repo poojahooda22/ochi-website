5c5255a9989fa619b683ea9a84b533a6b0c30b78
Pooja Hooda <91055527+poojahooda22@users.noreply.github.com>
2024-03-28 17:10:53 +0530

    updated eyes

--- src/components/Eyes.tsx
+++ src/components/Eyes.tsx
@@ -19,7 +19,7 @@ function Eyes() {
     })
   return (
     <div className="eyes w-full h-screen  overflow-hidden">
-      <div data-scroll data-scroll-speed="-.6" className="relative w-full h-full bg-cover bg-center bg-[url('https://ochi.design/wp-content/uploads/2022/05/Top-Viewbbcbv-1-scaled.jpg')] ">
+      <div data-scroll data-scroll-speed="-.7" className="relative w-full h-full bg-cover bg-center bg-[url('https://ochi.design/wp-content/uploads/2022/05/Top-Viewbbcbv-1-scaled.jpg')] ">
             <div className="absolute flex gap-10 items-center justify-center top-1/2 left-1/2 -translate-x-[50%] -translate-y-[50%]   ">
                  {/* This is the first eye */}
                 <div className="flex items-center justify-center w-[15vw] h-[15vw] rounded-full bg-zinc-100">
