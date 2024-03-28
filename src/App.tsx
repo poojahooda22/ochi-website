bcfa4f38f0720cb68c2dccec531188cb817d0e15
Pooja Hooda <91055527+poojahooda22@users.noreply.github.com>
2024-03-28 21:59:32 +0530

    updated app

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,29 @@
 
 import './App.css'
-import Navbar from './components/Navbar'
-// import SmoothScroll from './components/SmoothScroll';
-import { useRef } from 'react';
-import Cursor from "./components/StickyCursor/Index";
+import Navbar from './components/Navbar/Index';
+
+
 import styles from './page.module.scss';
 import LandingPage from './components/LandingPage';
 import Marqueue from './components/Marqueue';
 import About from './components/About';
 import Eyes from './components/Eyes';
 import Featured from './components/Featured';
-import LocomotiveScroll from 'locomotive-scroll';
+import SmoothScroll from './components/SmoothScroll';
 
 
 function App() {
-  const locomotiveScroll = new LocomotiveScroll();
-
-  const stickyElement = useRef(null);
   return (
     <html >
       <body className={styles.main}>
-          <Cursor stickyElement={stickyElement} />
-          <Navbar ref={stickyElement} />
+        <SmoothScroll>
+          <Navbar />
           <LandingPage />
           <Marqueue />
           <About />
           <Eyes />
           <Featured />
-        
+        </SmoothScroll>  
       </body>
     </html>
     
