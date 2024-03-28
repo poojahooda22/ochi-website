bbca067ce6876e82e47024af6873eef5c03997c7
Pooja Hooda <91055527+poojahooda22@users.noreply.github.com>
2024-03-28 16:21:01 +0530

    updated text

--- src/components/Featured.tsx
+++ src/components/Featured.tsx
@@ -10,13 +10,14 @@ function Featured() {
             <div className='cards w-full flex gap-10 '>
                 
                 <div className='cardcontainer relative w-1/2 h-[75vh]'>
-                    <h1 className='absolute left-full -translate-x-1/2 -translate-y-1/2 text-emerald-600 top-1/2 text-[5vw] font-[Roboto] tracking-tight leading-none z-[9]'>FYDE</h1>
+                    <h1 className='absolute left-full -translate-x-1/2 -translate-y-1/2 text-[#CDEA68] top-1/2 text-[5vw] font-[Roboto] tracking-tight leading-none z-[9]'>FYDE</h1>
                     <div className='card rounded-lg w-full h-full overflow-hidden'>
                         <img className='w-full h-full bg-cover' src="https://ochi.design/wp-content/uploads/2023/10/Fyde_Illustration_Crypto_2-1326x1101.png" />
                     </div>
                 </div>
                 <div className='cardcontainer relative w-1/2 h-[75vh]'>
-                    <div className='card rounded-lg w-full h-full overflow-hidden'>    
+                    <div className='card rounded-lg w-full h-full overflow-hidden'> 
+                    <h1 className='absolute left-full -translate-x-1/2 -translate-y-1/2 text-[#CDEA68] top-1/2 text-[5vw] font-[Roboto] tracking-tight leading-none z-[9]'>VISE</h1>   
                         <img className='w-full h-full bg-cover' src="https://ochi.design/wp-content/uploads/2022/09/Vise_front2-1326x1101.jpg" />
                     </div>
                 </div>
