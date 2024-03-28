0cdddbae79357426476042bf826163f7c29e3e2a
Pooja Hooda <91055527+poojahooda22@users.noreply.github.com>
2024-03-28 13:43:38 +0530

    updated scroll

--- src/components/Marqueue.tsx
+++ src/components/Marqueue.tsx
@@ -2,7 +2,7 @@ import {motion} from 'framer-motion';
 
 function Marqueue() {
   return (
-    <div data-scroll data-scroll-speed=".1" className='w-full py-20 rounded-tl-3xl rounded-tr-3xl bg-[#004D43]'>
+    <div data-scroll data-scroll-section data-scroll-speed=".1" className='w-full py-20 rounded-tl-3xl rounded-tr-3xl bg-[#004D43]'>
       <div className="text border-t-2 border-b-2 border-zinc-300 flex  overflow-hidden whitespace-nowrap">
 
         <motion.h1 
