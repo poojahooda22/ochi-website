08e9f1b0ff256c3b32245f77da389a8c3247f9fd
Pooja Hooda <91055527+poojahooda22@users.noreply.github.com>
2024-03-28 13:42:29 +0530

    updated marqueue

--- src/components/Marqueue.tsx
+++ src/components/Marqueue.tsx
@@ -2,14 +2,14 @@ import {motion} from 'framer-motion';
 
 function Marqueue() {
   return (
-    <div className='w-full py-20 rounded-tl-3xl rounded-tr-3xl bg-[#004D43]'>
+    <div data-scroll data-scroll-speed=".1" className='w-full py-20 rounded-tl-3xl rounded-tr-3xl bg-[#004D43]'>
       <div className="text border-t-2 border-b-2 border-zinc-300 flex  overflow-hidden whitespace-nowrap">
 
         <motion.h1 
           initial={{x: "0"}}
           animate={{x: "-100%"}}
           transition={{ repeat: Infinity, ease: "linear", duration: 5}}
-          className="uppercase font-bold text-[20vw] leading-none tracking-tighter mr-20
+          className="uppercase font-bold text-[20vw] font-[Roboto] leading-none tracking-normal mr-10
           text-white "
         >
           We are ochi
@@ -18,7 +18,16 @@ function Marqueue() {
           initial={{x: "0"}}
           animate={{x: "-100%"}}
           transition={{ repeat: Infinity, ease: "linear", duration: 5}}
-          className="uppercase font-bold text-[20vw] leading-none tracking-tighter mr-20
+          className="uppercase font-bold text-[20vw] font-[Roboto] leading-none tracking-normal mr-10
+          text-white "
+        >
+          We are ochi
+        </motion.h1>
+        <motion.h1 
+          initial={{x: "0"}}
+          animate={{x: "-100%"}}
+          transition={{ repeat: Infinity, ease: "linear", duration: 5}}
+          className="uppercase font-bold text-[20vw] font-[Roboto] leading-none tracking-normal mr-10
           text-white "
         >
           We are ochi
