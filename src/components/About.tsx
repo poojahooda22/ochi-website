3abfe4b64cf4cb7273586066967e54c3b4b67e14
Pooja Hooda <91055527+poojahooda22@users.noreply.github.com>
2024-03-29 14:25:57 +0530

    updated

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -14,12 +14,12 @@ function About() {
 
         <div className="w-full border-t-[1px] mt-24 border-gray-600 flex justify-between">
             <div className='mt-4'>
-                <h2 className='text-4xl mb-4'>Our approach</h2>
+                <h2 className='text-5xl mb-4'>Our approach</h2>
                 <div className='w-1/2'>
-                    <h4 className={`${styles.heading4} relative font-medium border-[1px] border-black text-[12px] py-[8px]  px-[12px] text-wrap text-center
+                    <h4 className={`${styles.heading4} relative font-medium border-[1px] border-black text-[1vw] py-[8px]  px-[12px] text-wrap text-center
                     bg-[#101112] text-[#fff]
                     rounded-[50px] overflow-hidden`}>
-                        <a className={`${styles.atag} `}>Read More</a>
+                        <a className={`${styles.atag}  `}>Read More</a>
                     </h4>
                 </div>
             </div>
