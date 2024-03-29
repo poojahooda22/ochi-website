36408e34ccce637789fa8ea950b3965a61709461
Pooja Hooda <91055527+poojahooda22@users.noreply.github.com>
2024-03-29 18:57:11 +0530

    updated heading

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -7,9 +7,9 @@ function About() {
     <div data-scroll data-scroll-section data-scroll-speed="-.1"
     className="w-full py-20 px-8 bg-[#CDEA68] rounded-tl-3xl rounded-tr-3xl">
         <h1 className="text-[4vw] leading-[4.5vw] tracking-tight">
-        Ochi is a strategic partner for fast-grow­ing tech 
-        businesses that need to raise funds, sell prod­ucts, ex­plain com­plex ideas,
-        and hire great peo­ple.
+            Ochi is a strategic partner for fast-grow­ing tech 
+            businesses that need to raise funds, sell prod­ucts, ex­plain com­plex ideas,
+            and hire great peo­ple.
         </h1>
 
         <div className="w-full border-t-[1px] mt-24 border-gray-600 flex justify-between ">
@@ -22,7 +22,7 @@ function About() {
                         bg-[#101112] text-[#fff]
                         rounded-[50px] overflow-hidden`}
                     >
-                        <a className={`${styles.atag}  `}>Read More</a>
+                        <a className={`${styles.atag}`}>Read More</a>
                     </h4>
                 </div>
             </div>
