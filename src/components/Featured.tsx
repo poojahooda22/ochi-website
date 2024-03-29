9f19fb675d730b1f628183c17f3c35797b8b0e66
Pooja Hooda <91055527+poojahooda22@users.noreply.github.com>
2024-03-29 19:01:56 +0530

    add

--- src/components/Featured.tsx
+++ src/components/Featured.tsx
@@ -60,7 +60,7 @@ function Featured() {
                 >
                     <div className='card rounded-lg w-full h-full overflow-hidden'> 
                         <h1 className='absolute flex right-full translate-x-1/2 overflow-hidden -translate-y-1/2 text-[#CDEA68] 
-                        top-1/2 text-[6vw] font-semibold font-[Roboto] tracking-wide leading-none z-[9]'>
+                            top-1/2 text-[6vw] font-semibold font-[Roboto] tracking-wide leading-none z-[9]'>
                             {"VISE".split('').map((item, index) => 
                                 (<motion.span 
                                     key={index}
