f2cb0d340afdcb66e890b63800e48bad92cba1f8
Pooja Hooda <91055527+poojahooda22@users.noreply.github.com>
2024-03-29 19:05:25 +0530

    updated marqueue

--- src/components/Marqueue.tsx
+++ src/components/Marqueue.tsx
@@ -4,7 +4,6 @@ function Marqueue() {
   return (
     <div data-scroll data-scroll-section data-scroll-speed=".1" className='w-full py-20 rounded-tl-3xl rounded-tr-3xl bg-[#004D43]'>
       <div className="text border-t-2 border-b-2 border-zinc-300 flex  overflow-hidden whitespace-nowrap">
-
         <motion.h1 
           initial={{x: "0"}}
           animate={{x: "-100%"}}
@@ -31,8 +30,7 @@ function Marqueue() {
           text-white "
         >
           We are ochi
-        </motion.h1>
-        
+        </motion.h1>  
       </div>
     </div>
   )
