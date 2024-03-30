41675dafa77fa65870cb159d50f934efb412bd0a
Pooja Hooda <91055527+poojahooda22@users.noreply.github.com>
2024-03-30 20:46:39 +0530

    add

--- src/components/Eyes.tsx
+++ src/components/Eyes.tsx
@@ -29,7 +29,8 @@ function Eyes() {
                 top-1/2 left-1/2 -translate-x-[50%] -translate-y-[50%]"
             >
                  {/* This is the first eye */}
-                <div className="flex items-center justify-center w-[15vw] h-[15vw] rounded-full bg-zinc-100">
+                <div 
+                className="flex items-center justify-center w-[15vw] h-[15vw] rounded-full bg-zinc-100">
                 <div  className="relative bg-zinc-900 w-[10vw] h-[10vw]  rounded-full">
                         <div 
                             style={{transform: `translate(-50%, -50%) rotate(${rotate}deg)`}} 
