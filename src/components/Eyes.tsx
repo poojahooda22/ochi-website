061d0232e6635bf537e440b01d3e1afa7495309e
Pooja Hooda <91055527+poojahooda22@users.noreply.github.com>
2024-03-30 20:46:30 +0530

    add

--- src/components/Eyes.tsx
+++ src/components/Eyes.tsx
@@ -26,7 +26,7 @@ function Eyes() {
         >
             <div 
                 className="absolute flex gap-10 items-center justify-center 
-                top-1/2 left-1/2 -translate-x-[50%] -translate-y-[50%]   "
+                top-1/2 left-1/2 -translate-x-[50%] -translate-y-[50%]"
             >
                  {/* This is the first eye */}
                 <div className="flex items-center justify-center w-[15vw] h-[15vw] rounded-full bg-zinc-100">
