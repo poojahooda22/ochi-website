9a504144e339d6d4efbe86058e8f987eba898800
Pooja Hooda <91055527+poojahooda22@users.noreply.github.com>
2024-07-27 14:37:43 +0530

    updated

--- src/components/Navbar/index.tsx
+++ src/components/Navbar/index.tsx
@@ -37,7 +37,7 @@ function Navbar () {
           <path d="M52.4097 10.1387C51.2512 10.1119 50.1066 10.3947 49.0941 10.958C48.0816 11.5212 47.2379 12.3445 46.6501 13.3427V0.172852H42.293V29.4688H46.6501C46.6501 29.1721 46.6501 18.7816 46.6501 18.7816C46.6501 15.6946 47.8619 13.4352 50.8084 13.4352C54.6046 13.4352 54.6209 17.4178 54.6209 19.6962C54.6209 22.9165 54.6209 25.5189 54.6209 28.7393V29.4987H59.0271C59.0271 29.3708 59.0488 29.2728 59.0488 29.1721C59.0488 25.5108 59.0951 21.8522 59.0325 18.1909C58.9916 15.6538 58.5015 10.1387 52.4097 10.1387Z" fill="currentColor"></path>
         </svg>
       </div>
-      <div className="links flex gap-6 items-center z-[9] ">
+      <div className="hidden links flex gap-6 items-center z-[9] ">
         {["Services", "Our work", "About us", "Insights"].map((item, index) => (
           <h4 key={index} className={`${styles.heading4} relative font-medium text-[.7vw] sm px-[18px] 
           py-[8px] border-[1px] border-[#111] rounded-[50px] overflow-hidden`}
