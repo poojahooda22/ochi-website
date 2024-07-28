edb3a2e45be01e09a79477af2d99ef76b43ef8e0
Pooja Hooda <91055527+poojahooda22@users.noreply.github.com>
2024-07-28 21:47:00 +0530

    updated page

--- src/components/LandingPage.tsx
+++ src/components/LandingPage.tsx
@@ -51,7 +51,8 @@ function LandingPage() {
                 py-[8px] border-[1px] border-[#111] rounded-[50px] overflow-hidden`}
             >
                 <a className={`${styles.atag} text-gray-900`}>
-                    Start project</a>
+                    Start project
+                    </a>
             </h4>
         </div>
       </div>
