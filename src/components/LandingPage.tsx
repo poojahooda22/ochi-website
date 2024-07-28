5528b4465790170f451bdffa1ca14815ab4f93af
Pooja Hooda <91055527+poojahooda22@users.noreply.github.com>
2024-07-28 21:21:08 +0530

    updated landing page

--- src/components/LandingPage.tsx
+++ src/components/LandingPage.tsx
@@ -34,7 +34,7 @@ function LandingPage() {
       </div>
 
         <div 
-        className="border-t-[1px] border-zinc-400 mt-[18vh] 
+            className="border-t-[1px] border-zinc-400 mt-[18vh] 
             flex flex-col items-start gap-[6vw] sm:flex 
             justify-between sm:items-center py-4 px-4 sm:px-8 "
         >
